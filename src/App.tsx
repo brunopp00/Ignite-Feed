7277b50416a6e3b9564ac4a5cc92c7c0dd7a423e
brunopp00 <brunofnh08@gmail.com>
2023-01-16 21:20:45 -0300

    teste

---
+++ src/App.tsx
@@ -0,0 +1,66 @@
+import { Header } from './components/Header'
+import { Post } from './components/Post'
+import { Sidebar } from './components/Sidebar'
+
+import './global.css'
+
+import styles from './App.module.css'
+
+const posts = [
+  {
+    id:1,
+    author: {
+      avatarUrl: 'http://github.com/brunopp00.png',
+      name:'Bruno Frohlich',
+      role: 'Estagiario Unimed'
+    },
+    content: [
+        {type: 'paragraph', content: 'Fala galeraa 👋'},
+        {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+        {type: 'link', content: 'jane.design/doctorcare'},
+        {type: 'link', content:'#novoprojeto'},
+        {type: 'link', content:'#nlw'},
+        {type: 'link', content:'#rocketseat'}
+    ],
+    publisheAt: new Date('2023-01-13 20:00:00')
+  },
+  {
+    id:2,
+    author: {
+      avatarUrl: 'http://github.com/diego3g.png',
+      name:'Diego Fernandes',
+      role: 'Professor Rocketseat'
+    },
+    content: [
+        {type: 'paragraph', content: 'Fala galeraa 👋'},
+        {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+        {type: 'link', content: 'jane.design/doctorcare'},
+        {type: 'link', content:'#novoprojeto'},
+        {type: 'link', content:'#nlw'},
+        {type: 'link', content:'#rocketseat'}
+    ],
+    publisheAt: new Date('2023-01-15 20:00:00')
+  }
+]
+
+export function App() {
+  return (
+    <div>
+      <Header/>
+      
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          {posts.map(post => (
+            <Post
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publisheAt={post.publisheAt}
+            />
+          ))}
+        </main>
+      </div> 
+    </div>
+  )
+}
---
+++ src/components/Post.tsx
@@ -0,0 +1,104 @@
+import { Avatar } from './Avatar'
+import { Comment } from './Comment'
+import styles from './Post.module.css'
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+import { FormEvent, InvalidEvent, useState } from 'react'
+
+interface Content {
+    type: 'paragraph' | 'link';
+    content: string;
+}
+
+interface PostProps {
+    author: {
+        name: string;
+        avatarUrl: string;
+        role: string;
+    };
+    publisheAt: Date;
+    content: Content[];   
+}
+
+export function Post({author, publisheAt, content}: PostProps) {
+
+    const [coments, setComents] = useState([
+        'Post muito bacana, hein?!'
+    ])
+
+    const [newCommentText, setNewCommentText] = useState('')
+
+    const publishedDateAndFormatted = format(publisheAt, "d 'de' LLLL 'às' HH:mm'h'", {
+        locale:ptBR,
+    } )
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publisheAt, {
+        locale: ptBR,
+        addSuffix:true,
+    })
+    
+    function handleCreateNewComment(event: FormEvent) {
+        event.preventDefault()
+        setComents([...coments, newCommentText])
+        setNewCommentText('')
+    }
+
+    function handleInvalidComment(event: InvalidEvent<HTMLTextAreaElement>) {
+        event.target.setCustomValidity('Esse campo é obrigatório!')
+    }
+
+    function deleteComment(commentToDelete:string) {
+        const commentsWithoutDeleteOne = coments.filter(comment => {
+            return comment !== commentToDelete
+        })
+        setComents(commentsWithoutDeleteOne)
+    }
+
+    return(
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <Avatar src={author.avatarUrl} />
+                    <div className={styles.authorInfo}>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
+                    </div>
+                </div>
+                <time title={publishedDateAndFormatted} dateTime={publisheAt.toISOString()}>{publishedDateRelativeToNow}</time>
+            </header>
+            <div className={styles.content}>
+                {content.map(line => {
+                    if(line.type === 'paragraph'){
+                        return <p key={line.content}>{line.content}</p>
+                    } else if(line.type === 'link'){
+                        <p key={line.content}><a href="">{line.content}</a></p>
+                    }
+                })}
+            </div>
+            <form onSubmit={(e) => handleCreateNewComment(e)} className={styles.commentForm}>
+                
+                <strong>Deixe seu feedback</strong>
+                <textarea
+                    required
+                    value={newCommentText}
+                    name='comment'
+                    onChange={(e) => {
+                        e.target.setCustomValidity('')
+                        setNewCommentText(e.target.value)
+                    }}
+                    placeholder='Deixe seu comentário'
+                    onInvalid={handleInvalidComment}
+                />
+                <footer>
+                    <button disabled={newCommentText === ''} type='submit'>Publicar</button>
+                
+                </footer>
+            </form>
+            <div className={styles.commentList}>
+                {coments.map((comment) => (
+                    <Comment key={comment} content={comment} onDeleteComment={deleteComment} />
+                ))}
+            </div>
+        </article>  
+    )
+}
